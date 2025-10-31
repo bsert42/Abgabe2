c5b23ed2470afdf54d24c931050775617d13f321
bsert42 <bsert@smail.uni-koeln.de>
2025-10-31 23:21:23 +0100

    Add files via upload

---
+++ src/hau1/HA1.java
@@ -0,0 +1,14 @@
+package ha1;
+
+public class HA1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int z = 17;
+		if (z>10) {
+			z-=10;
+		}
+		System.out.println (z);
+	}
+
+}
