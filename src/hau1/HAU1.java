8fb50f201cc47600bbaef6a61196fb0cd6616b93
bsert42 <bsert@smail.uni-koeln.de>
2025-10-31 23:25:44 +0100

    Update HAU1.java

--- src/hau1/HAU1.java
+++ src/hau1/HAU1.java
@@ -4,7 +4,11 @@ public class HAU1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int z = 17;
+		if (z>10) {
+			z-=10;
+		}
+		System.out.println (z);
 	}
 
 }
